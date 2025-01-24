ddb29ed28790eb651fa88168862359590400a109
michael pizarro <michael.pizarro.l@gmail.com>
2025-01-24 15:56:32 -0300

    Agregando funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombres = [];
+
+function agregarAmigo(){
+    let entrada = document.getElementById("amigo");
+    let nombre = entrada.value.trim();
+
+    if(nombre === ""){
+        alert("Por favor, inserte un nombre");
+    }else{
+        nombres.push(nombre);
+        console.log(nombres);
+        entrada.value = ""; //Se limpia el placeholder
+    }
+}
