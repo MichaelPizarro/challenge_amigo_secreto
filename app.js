f990a433e9ae5e4d9f7edca993ca713cd621db55
michael pizarro <michael.pizarro.l@gmail.com>
2025-01-24 16:21:57 -0300

    Agregamos funcion actualizar lista

--- app.js
+++ app.js
@@ -2,15 +2,30 @@
 
 let nombres = [];
 
-function agregarAmigo(){
+function actualizarLista() {
+    let lista = document.getElementById("resultado");
+
+    lista.innerHTML = ""; // Limpiamos la lista
+
+   
+    nombres.forEach(nombre => {
+        lista.innerHTML += `<li>${nombre}</li>`;
+    });
+}
+
+function agregarAmigo() {
     let entrada = document.getElementById("amigo");
     let nombre = entrada.value.trim();
 
-    if(nombre === ""){
+    if (nombre === "") {
         alert("Por favor, inserte un nombre");
-    }else{
-        nombres.push(nombre);
-        console.log(nombres);
-        entrada.value = ""; //Se limpia el placeholder
+    } else {
+        nombres.push(nombre); 
+        console.log(nombres); 
+        entrada.value = ""; // Limpiamos el input
+        actualizarLista(); 
     }
 }
+
+
+
